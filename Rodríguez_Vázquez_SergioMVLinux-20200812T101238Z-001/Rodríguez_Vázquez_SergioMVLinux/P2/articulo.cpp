003ad02443a8ea8848383d84079d5d94bbf1d0e9
Sergio Rodríguez <sergio.rodriguezvazquez@alum.uca.es>
2020-08-25 13:56:07 +0200

    Ningun problema con la clase articulo, pasa los tests

--- Rodríguez_Vázquez_SergioMVLinux-20200812T101238Z-001/Rodríguez_Vázquez_SergioMVLinux/P2/articulo.cpp
+++ Rodríguez_Vázquez_SergioMVLinux-20200812T101238Z-001/Rodríguez_Vázquez_SergioMVLinux/P2/articulo.cpp
@@ -1,11 +1,21 @@
 #include "articulo.hpp"
+#include "fecha.hpp"
+#include "cadena.hpp"
+#include <iomanip>
+#include <stdlib.h>
+#include <iostream>
+#include <locale>
 
-Articulo::Articulo(const Cadena referencia, const Cadena titulo, const Fecha &f, double precio, unsigned numeroejemplar = 0): referencia_(referencia), titulo_(titulo), publicacion_(f), precio_(precio), ejemplares_(numeroejemplar)
+
+//CONSTRUCTOR
+Articulo::Articulo(const Cadena& referencia, const Cadena& titulo, const Fecha &f, double precio, unsigned numeroejemplar = 0): referencia_(referencia), titulo_(titulo), f_publi_(f), precio_(precio), stock_(numeroejemplar)
 {}
 
-std::ostream &operator<<(std::ostream &os, const Articulo &A)
+
+//OPERADOR SALIDA
+std::ostream &operator<<(std::ostream &os, const Articulo &A) noexcept
 {
-    os << "[" << A.referencia() << "] \"" << A.titulo() << "\", " << A.f_pub().anno() << ". " << A.precio() << " €";
+    os << "[" << A.referencia() << "]\"" << A.titulo() << "\", " << A.f_publi().anno() << ". " << A.precio() << " €";
 
     return os;
 }
--- Rodríguez_Vázquez_SergioMVLinux-20200812T101238Z-001/Rodríguez_Vázquez_SergioMVLinux/P2/articulo.hpp
+++ Rodríguez_Vázquez_SergioMVLinux-20200812T101238Z-001/Rodríguez_Vázquez_SergioMVLinux/P2/articulo.hpp
@@ -3,29 +3,33 @@
 
 #include "cadena.hpp"
 #include "fecha.hpp"
-#include <iostream>
+//#include <iostream>
 
 class Articulo
 {
 public:
-    Articulo(const Cadena referencia, const Cadena titulo, const Fecha &f, double precio, unsigned numeroejemplar);
+    //CONSTRUCTOR
+    Articulo(const Cadena& referencia, const Cadena& titulo, const Fecha &f, double precio, unsigned numeroejemplar);
 
+    //OBSERVADORES
     Cadena referencia() const noexcept {return referencia_;};
     Cadena titulo() const noexcept {return titulo_;};
-    Fecha f_pub() const noexcept {return publicacion_;};
+    Fecha f_publi() const noexcept {return f_publi_;};
     double precio() const noexcept {return precio_;};
     double &precio() noexcept {return precio_;};
-    unsigned ejemplares() const noexcept {return ejemplares_;};
-    unsigned &ejemplares() noexcept {return ejemplares;};
+    unsigned stock() const noexcept {return stock_;};
+    unsigned &stock() noexcept {return stock_;};
 
 private:
     Cadena referencia_;
     Cadena titulo_;
-    Fecha publicacion_;
+    Fecha f_publi_;
     double precio_;
-    unsigned ejemplares_;
+    unsigned stock_;
 };
 
-std::ostream &operator<<(std::ostream &os, const Articulo &A);
+
+//OPERADOR SALIDA
+std::ostream &operator<<(std::ostream &os, const Articulo &A) noexcept;
 
 #endif //ARTICULO_HPP
